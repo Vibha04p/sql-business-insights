ecd3b34fdab1c84a3a066a8608926f80863944f0
Vibha V Pamarti <161676329+Vibha04p@users.noreply.github.com>
2026-07-01 21:18:49 +0530

    Update 01_daily_business_summary.sql

--- queries/01_daily_business_summary.sql
+++ queries/01_daily_business_summary.sql
@@ -1,1 +1,8 @@
--- Query will be added later.
+-- Q1: Daily Business Summary with DoD and Same-Weekday WoW
+-- Business Question: How are we doing today vs yesterday, and vs the same day last week?
+-- Owner: Vibha P
+-- Last Updated: 2026-07-01
+-- Sanity Check:
+-- 1. paid_order_rate should always be between 0 and 1.
+-- 2. Sum of daily orders should equal total orders in the selected date range.
+
